18be52783bcd5ebb254b2fd96e4b589203fb077f
sebnalli <nallisebastian@gmail.com>
2026-09-19 22:02:37 -0400

    Add mine asset rendering to GUI

--- src/gui/main.py
+++ src/gui/main.py
@@ -1,12 +1,12 @@
 import pygame
 from src.core.cell import Cell
-from src.core.board import create_board
-from src.core.game import reveal_cell
+from src.core.board import create_board, mine_placement, calculate_touching_mines
+from src.core.game import reveal_cell, reveal_empty_area
 
 pygame.init()
 
 # Board and tile dimensions
-TILE_SIZE = 40
+TILE_SIZE = 70
 BOARD_SIZE = 9
 
 WIDTH = TILE_SIZE * BOARD_SIZE
@@ -30,13 +30,21 @@ revealed_dark = pygame.image.load(
     "src/gui/assets/revealed_tile_dark.png"
 ).convert_alpha()
 
+# Mine asset
+mine_image = pygame.image.load(
+    "src/gui/assets/mine.png"
+).convert_alpha()
+
 # Scale assets to match tile size
 hidden_tile = pygame.transform.scale(hidden_tile, (TILE_SIZE, TILE_SIZE))
 revealed_light = pygame.transform.scale(revealed_light, (TILE_SIZE, TILE_SIZE))
 revealed_dark = pygame.transform.scale(revealed_dark, (TILE_SIZE, TILE_SIZE))
+mine_image = pygame.transform.scale(mine_image, (TILE_SIZE, TILE_SIZE))
 
 # Create board
 board = create_board(Cell)
+
+first_click = True
 
 running = True
 
@@ -53,6 +61,12 @@ while running:
             row = mouse_y // TILE_SIZE
 
             print(f"Row: {row}, Column: {column}")
+
+            # Handles first-click safety, places mines, and assigns touching-mine counts
+            if first_click:
+                mine_placement(board, row, column)
+                calculate_touching_mines(board)
+                first_click = False
 
             # Reveal the tile
             reveal_cell(board, row, column)
@@ -74,6 +88,9 @@ while running:
                     screen.blit(revealed_light, (x, y))
                 else:
                     screen.blit(revealed_dark, (x, y))
+
+                if current_cell.mine:
+                    screen.blit(mine_image, (x, y))
 
             # Otherwise display a hidden tile
             else:
